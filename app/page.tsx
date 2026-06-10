f05365815101c37ea77e1df2bf2ed1a6ea9d8e55
Tomas <tomptacnik@gmail.com>
2026-06-10 16:21:01 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -490,7 +490,6 @@ useEffect(() => {
   /* LOGIN FLOW (beze změny) */
   <>
     {/* LOGIN OPTIONS */}
-    {/* LOGIN OPTIONS */}
 <div
   className={`
     overflow-hidden transition-all duration-300 ease-out
@@ -502,29 +501,47 @@ useEffect(() => {
     }
   `}
 >
-  <div className="flex flex-col gap-2 pb-2">
+  <div className="flex flex-col gap-3 pb-3">
 
-    {/* GOOGLE TLAČÍTKO – Odpovídá Google Branding Guidelines */}
+    {/* GOOGLE TLAČÍTKO – 100% shoda s oficiálním generátorem */}
     <button
       onClick={signInWithGoogle}
       type="button"
-      className="w-full h-10 px-3 bg-white border border-[#747775] text-[#1f1f1f] text-sm font-medium rounded-2xl cursor-pointer flex items-center justify-between relative overflow-hidden transition-all duration-200 hover:bg-[#303030]/[0.08] hover:shadow-[0_1px_2px_0_rgba(60,64,67,0.3),0_1px_3px_1px_rgba(60,64,67,0.15)] active:bg-[#303030]/[0.12] focus:outline-none"
+      className="group relative h-10 px-3 min-w-max max-w-[400px] w-full bg-white border border-[#747775] rounded-[4px] text-[#1f1f1f] text-sm tracking-[0.25px] text-center select-none cursor-pointer overflow-hidden whitespace-nowrap align-middle outline-none transition-all duration-[0.218s]"
       style={{ fontFamily: "'Roboto', arial, sans-serif" }}
     >
-      <div className="w-5 h-5 flex-shrink-0 flex items-center justify-center mr-2.5">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className="w-5 h-5 block">
-          <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
-          <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
-          <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
-          <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
-        </svg>
+      {/* Stavová vrstva pro hover/active efekty (.gsi-material-button-state) */}
+      <div className="absolute inset-0 bg-[#303030] opacity-0 transition-opacity duration-[0.218s] group-hover:opacity-[8%] group-active:opacity-[12%] group-focus:opacity-[12%]" />
+      
+      {/* Stín při hoveru (.gsi-material-button:not(:disabled):hover) */}
+      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-[0.218s] group-hover:shadow-[0_1px_2px_0_rgba(60,64,67,0.3),0_1px_3px_1px_rgba(60,64,67,0.15)] pointer-events-none rounded-[3px]" />
+
+      {/* Wrapper obsahu (.gsi-material-button-content-wrapper) */}
+      <div className="relative w-full h-full flex items-center justify-between flex-nowrap">
+        
+        {/* Ikona (.gsi-material-button-icon) */}
+        <div className="w-5 h-5 min-w-[20px] mr-2.5 flex items-center justify-center">
+          <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" style={{ display: "block" }} className="w-full h-full">
+            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+            <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+            <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+            <path fill="none" d="M0 0h48v48H0z"></path>
+          </svg>
+        </div>
+
+        {/* Text (.gsi-material-button-contents) */}
+        <span 
+          className="flex-grow font-medium text-left overflow-hidden text-ellipsis align-top"
+          style={{ fontFamily: "'Roboto', arial, sans-serif" }}
+        >
+          Pokračovat přes Google
+        </span>
+        
       </div>
-      <span className="flex-grow text-center tracking-wide font-medium">
-        Pokračovat přes Google
-      </span>
     </button>
 
-    {/* SEZNAM TLAČÍTKO – Využívá SVG z public složky */}
+    {/* SEZNAM TLAČÍTKO – Vzhledově přizpůsobené layoutu */}
     <button 
       type="button"
       className="w-full h-10 px-3 bg-white border border-gray-200 text-gray-800 text-sm font-medium rounded-2xl cursor-pointer flex items-center justify-between transition-all duration-200 hover:bg-gray-50 focus:outline-none"
